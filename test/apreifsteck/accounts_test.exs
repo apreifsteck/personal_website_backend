1a5a9953f9e7f45bdec628046c9f7c86d8bd7666
Austin Reifsteck <apreifsteck@gmail.com>
2020-05-17 00:08:30 -0400

    cleaned up old code in accounts test

--- test/apreifsteck/accounts_test.exs
+++ test/apreifsteck/accounts_test.exs
@@ -5,60 +5,18 @@ defmodule APReifsteck.AccountsTest do
 
   alias APReifsteck.Accounts.User
 
-  @valid_attrs %{
-    email: "some email",
-    name: "some name",
-    password: "some password",
-    uname: "some uname"
-  }
   @update_attrs %{
     email: "some updated email",
     name: "some updated name",
+    # I cheated and looked
+    current_password: "secretpassword",
     password: "some updated password",
+    password_confirmation: "some updated password",
     uname: "some updated uname"
   }
   @invalid_attrs %{email: nil, name: nil, password: nil, uname: nil}
 
-  describe "create_user/1" do
-    test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{id: id} = user} = Accounts.create_user(@valid_attrs)
-      assert user.email == "some email"
-      assert user.name == "some name"
-      assert user.uname == "some uname"
-      assert [%User{id: ^id}] = Accounts.list_users()
-    end
-
-    test "create_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
-    end
-
-    test "enforces unique usernames" do
-      assert {:ok, %User{id: id}} = Accounts.create_user(@valid_attrs)
-      assert {:error, changeset} = Accounts.create_user(@valid_attrs)
-
-      assert %{uname: ["has already been taken"]} = errors_on(changeset)
-
-      assert [%User{id: ^id}] = Accounts.list_users()
-    end
-
-    test "does not accept long usernames" do
-      attrs = Map.put(@valid_attrs, :uname, String.duplicate("a", 30))
-      {:error, changeset} = Accounts.create_user(attrs)
-
-      assert %{uname: ["should be at most 20 character(s)"]} = errors_on(changeset)
-
-      assert Accounts.list_users() == []
-    end
-
-    test "requires password to be at least 6 chars long" do
-      attrs = Map.put(@valid_attrs, :password, "12345")
-      {:error, changeset} = Accounts.create_user(attrs)
-
-      assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
-
-      assert Accounts.list_users() == []
-    end
-  end
+  # User creation now handled by registration controller
 
   test "list_users/0 returns all users" do
     user = user_fixture()
